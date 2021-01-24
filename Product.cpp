bcc258241a316cc36fa9ea706abf1ce3b8bd254c
SrivathsanKannan <67915515+SrivathsanKannan@users.noreply.github.com>
2021-01-24 21:13:35 +0400

    Created Product.cpp

---
+++ Product.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b;
+	cout<<"Enter the first number "<<endl;
+	cin>>a;
+	cout<<"Enter the second number "<<endl;
+	cin>>b;
+	cout<<"the product of the two given numbers are "<<a*b;
+	return 0;
+}
